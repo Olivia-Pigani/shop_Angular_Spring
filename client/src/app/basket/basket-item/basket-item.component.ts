b75afc7d9c2e7ded82569b7248ea5e2fd66ea77e
Olivia-Pigani <piganiolivia@gmail.com>
2024-11-06 17:10:51 +0100

    can update quantity in basket item

--- client/src/app/basket/basket-item/basket-item.component.ts
+++ client/src/app/basket/basket-item/basket-item.component.ts
@@ -3,11 +3,12 @@ import { BasketItem } from '../basket-item';
 import { BasketService } from '../basket.service';
 import { CommonModule } from '@angular/common';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgModel,FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-basket-item',
   standalone: true,
-  imports: [CommonModule,ReactiveFormsModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './basket-item.component.html',
   styleUrl: './basket-item.component.css'
 })
@@ -15,6 +16,10 @@ export class BasketItemComponent{
 private basketService:BasketService = inject(BasketService);
 @Input() basketItem!:BasketItem;
 @Input() index!:number;
+
+public onQuantitySelected(quantity:number):void{
+this.basketService.updateItemQuantity(this.basketItem,quantity)
+}
 
 public removeItem():void{
 this.basketService.removeToBasket(this.index)
