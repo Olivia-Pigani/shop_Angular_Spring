7c08e7ee50298c27c0e46a21c54087f799310013
Olivia-Pigani <piganiolivia@gmail.com>
2024-11-09 18:39:58 +0100

    address form done

--- client/src/app/customer/account-page/account-page.component.ts
+++ client/src/app/customer/account-page/account-page.component.ts
@@ -25,6 +25,8 @@ import { CustomerService } from '../customer.service';
 import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
 import { CommonModule, JsonPipe } from '@angular/common';
 import { AuthService } from '../../auth/auth.service';
+import { Address } from '../address';
+import { AddressService } from '../address.service';
 
 @Component({
   selector: 'app-account-page',
@@ -43,22 +45,36 @@ import { AuthService } from '../../auth/auth.service';
 export class AccountPageComponent implements OnInit {
   private formBuilder: FormBuilder = inject(FormBuilder);
   private customerService: CustomerService = inject(CustomerService);
+  private addressService: AddressService = inject(AddressService);
   private destroyRef: DestroyRef = inject(DestroyRef);
   public customer: Signal<Customer | undefined> = toSignal(
     this.customerService.customerDetails$
+  );
+  public address: Signal<Address | undefined> = toSignal(
+    this.addressService.addressDetails$
   );
   public pageTitle: string = 'My Account';
 
   //about customer form
   public customerInfoForm!: FormGroup;
   public initialCustomerData: Customer | undefined;
-  public isFormValid: boolean = true;
-  public isFormSubmitted: boolean = false;
+  public isCustomerFormValid: boolean = true;
+  public isCustomerFormSubmitted: boolean = false;
+
+  //about address form
+  public addressInfoForm!: FormGroup;
+  public initialAddressData: Address | undefined;
+  public isAddressFormValid: boolean = true;
+  public isAddressFormSubmitted: boolean = false;
 
   public displayCustomerDataEffect: EffectRef = effect(() => {
     this.initialCustomerData = this.customer();
     if (this.initialCustomerData) {
       this.updateFormWithCustomerData(this.initialCustomerData);
+    }
+    this.initialAddressData = this.address();
+    if (this.initialAddressData) {
+      this.updateFormWithAddressData(this.initialAddressData);
     }
   });
 
@@ -77,17 +93,24 @@ export class AccountPageComponent implements OnInit {
       ],
       email: ['', [Validators.required, Validators.email]],
     });
+
+    this.addressInfoForm = this.formBuilder.group({
+      country: ['', [Validators.required]],
+      city: ['', [Validators.required]],
+      street: ['', [Validators.required]],
+      houseNumber: ['', [Validators.required]],
+    });
   }
 
   public onSubmitCustomerInfo() {
     if (!this.customerInfoForm.valid) {
-      this.isFormValid = false;
+      this.isCustomerFormValid = false;
       return;
     }
     const updatedCustomerData = this.customerInfoForm.value as Customer;
 
-    if (this.isTheSameData(updatedCustomerData)) {
-      this.isFormValid = false;
+    if (this.isTheSameCustomerData(updatedCustomerData)) {
+      this.isCustomerFormValid = false;
       return;
     }
 
@@ -95,25 +118,63 @@ export class AccountPageComponent implements OnInit {
       .updateCustomerById(updatedCustomerData)
       .pipe(
         catchError(() => {
-          this.isFormValid = false;
+          this.isCustomerFormValid = false;
           return EMPTY;
         }),
         takeUntilDestroyed(this.destroyRef)
       )
       .subscribe(() => {
-        this.isFormValid = true;
-        this.isFormSubmitted = true;
+        this.isCustomerFormValid = true;
+        this.isCustomerFormSubmitted = true;
       });
   }
 
-  public closeSuccessMsg(): void {
-    this.isFormSubmitted = false;
+  
+  public onSubmitAddressInfo() {
+    if (!this.addressInfoForm.valid) {
+      this.isAddressFormValid = false;
+      return;
+    }
+    const updatedAddressData = this.addressInfoForm.value as Address;
+
+    if (this.isTheSameAddressData(updatedAddressData)) {
+      this.isAddressFormValid = false;
+      return;
+    }
+
+    this.addressService
+      .updateAddressByCustomerId(updatedAddressData)
+      .pipe(
+        catchError(() => {
+          this.isAddressFormValid = false;
+          return EMPTY;
+        }),
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe(() => {
+        this.isAddressFormValid = true;
+        this.isAddressFormSubmitted = true;
+      });
   }
 
-  private isTheSameData(updatedCustomerData: Customer): boolean {
+  public closeCustomerFormSuccessMsg(): void {
+    this.isCustomerFormSubmitted = false;
+  }
+  public closeAddressFormSuccessMsg(): void {
+    this.isAddressFormSubmitted = false;
+  }
+
+  private isTheSameCustomerData(updatedCustomerData: Customer): boolean {
     return (
       JSON.stringify(this.initialCustomerData) ===
       JSON.stringify(updatedCustomerData)
+    );
+  }
+
+  private isTheSameAddressData(updatedAddressData: Address): boolean {
+    return (
+      JSON.stringify(this.initialAddressData) ===
+      JSON.stringify(updatedAddressData)
     );
   }
 
@@ -124,6 +185,15 @@ export class AccountPageComponent implements OnInit {
       birthDate: customer.birthDate,
       phoneNumber: customer.phoneNumber,
       email: customer.email,
+    });
+  }
+
+  private updateFormWithAddressData(address: Address) {
+    this.addressInfoForm.patchValue({
+      country:address.country,
+      city:address.city,
+      street:address.street,
+      houseNumber:address.houseNumber
     });
   }
 }
---
+++ client/src/app/customer/address.service.ts
@@ -0,0 +1,49 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { HttpErrorService } from '../utils/http-error.service';
+import { Observable, catchError, tap, throwError } from 'rxjs';
+import { Address } from './address';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AddressService {
+private http:HttpClient = inject(HttpClient);
+private addressUrl:string = 'http://localhost:8586/api/v1/addresses';
+private httpErrorService:HttpErrorService = inject(HttpErrorService);
+private jwtHelper: JwtHelperService = inject(JwtHelperService);
+
+
+public addressDetails$: Observable<Address> = this.http.get<Address>(`${this.addressUrl}/customers/${this.customerId}`, {headers:this.header})
+.pipe(
+  tap(address => console.log(address)),
+  catchError(err=>
+    this.handleError(err))
+);
+
+public updateAddressByCustomerId(updatedAddressData: Address):Observable<Address>{
+  return this.http.put<Address>(`${this.addressUrl}/customers/${this.customerId}`,updatedAddressData, {headers: this.header});
+}
+
+private get customerId(): string | null {
+  const token = localStorage.getItem("token");
+  if (token) {
+    const decodedToken = this.jwtHelper.decodeToken(token);
+    return decodedToken?.userId ?? null;
+  }
+  return null;
+}
+
+private handleError(err: HttpErrorResponse):Observable<never>{
+  const formattedMsg = this.httpErrorService.formatError(err);
+  return throwError(()=>formattedMsg); 
+  }
+
+private get header(): HttpHeaders{
+ const token:string | null= localStorage.getItem("token"); 
+ return new HttpHeaders({
+  'Authorization': 'Bearer '+ token
+ })
+}
+}
